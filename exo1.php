47fe5ec1070a6388a89eb5d6cb91528f316e7937
dodomoumb <ing_doris2005@yahoo.es>
2018-09-13 16:32:55 +0200

    Close  #1

--- exo1.php
+++ exo1.php
@@ -1,4 +1,19 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href= "exo1.css" type = "text/css">
+</head>
+<body>
+    
+</body>
+</html>
+
+
 <?php
 
 $x = 155;
@@ -11,6 +26,19 @@ $rand = -1;
         $rand = rand(0, 1000);
         $test ++;
      } 
-     echo $test . ' fois '. $rand. '<br>';
+
+     if ($rand == $x) {
+    ?>
+             <br>
+             <div class ="exo1" > 
+                 <p> 
+                  <?php echo $test . ' fois '. $rand; ?>
+                 </p>
+              </div> 
+        <?php     
+     }
+   else  echo $test . ' fois '. $rand. '<br>';
+
+    
 
 ?>
